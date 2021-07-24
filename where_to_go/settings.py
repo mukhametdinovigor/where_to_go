b224d72567b3e8b10b211d722c4712a263f64f94
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-24 22:28:36 +1100

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
