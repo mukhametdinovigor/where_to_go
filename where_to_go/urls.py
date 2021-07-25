653027491d96208ddde719ed4b3cbfb00fd9c58e
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-26 05:47:00 +1100

    Change views.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -18,10 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-import views
+from places.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
