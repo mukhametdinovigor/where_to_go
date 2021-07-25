4e831ff24e2e5a7e22196e68a5206e4f87d9b67d
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-25 22:21:39 +1100

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,16 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    title = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField('Фото')
+
+    def __str__(self):
+        return f'{self.id} {str(self.title)}'
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
