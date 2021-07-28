1bcb96f52e1c6e727feafe298cfb52c89a3b9b40
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-29 07:46:40 +1100

    Update environment setup

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", 'django-insecure-k6gko1(bj#g(2wwf)l8vtcgr=09frdt17yq+!p*p8@1hd_&yx-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
+DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = env.str("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", '*')
 
 
 # Application definition
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env.str("DATABASE_NAME"),
+        'NAME': BASE_DIR / env.str("DATABASE_NAME", 'db.sqlite3'),
     }
 }
 
