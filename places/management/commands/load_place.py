e3d2eae7a963c560b5acb289c1393ebbd29e4721
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-07-30 08:08:14 +1100

    Add custom management command for loading places

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from django.core.management.base import BaseCommand
+from django.shortcuts import get_object_or_404
+from places.models import Place
+
+
+class Command(BaseCommand):
+    help = 'Upload place to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', type=str)
+
+    def download_image(self, image_url):
+        os.makedirs('media/downloaded_images', exist_ok=True)
+        img_name = os.path.basename(image_url)
+        img_path = os.path.join(os.getcwd(), 'media/downloaded_images', img_name)
+        response = requests.get(image_url)
+        with open(img_path, 'wb') as img:
+            img.write(response.content)
+        return img_path
+
+    def handle(self, *args, **kwargs):
+        place_url = kwargs['url']
+        response = requests.get(place_url)
+        response.raise_for_status()
+        place_attrs = response.json()
+        Place.objects.get_or_create(
+            title=response.json()['title'],
+            description_short=place_attrs['description_short'],
+            description_long=place_attrs['description_long'],
+            longitude=place_attrs['coordinates']['lng'],
+            latitude=place_attrs['coordinates']['lat']
+        )
+        img_order_field = 1
+        for img_url in response.json()['imgs']:
+            image = get_object_or_404(Place, title=response.json()['title']).images.\
+                get_or_create(title=response.json()['title'], order=img_order_field)
+            img_to_upload = open(self.download_image(img_url), 'rb')
+            image[0].image.save(os.path.basename(img_url), img_to_upload, save=True)
+            img_order_field += 1
